96284b4d9f9ce6500fced10d494e4bf25885bf6b
Joseph Chris <279212363@qq.com>
2019-06-16 23:04:34 +0800

    add function: cet4/6 grade calc

---
+++ cetcalc/autocalc.js
@@ -0,0 +1,94 @@
+var cet = {
+    400: 710,
+    410: 710*0.15,
+    421: 710*0.07,
+    422: 56.8,
+    423: 710*0.20,
+    431: 710*0.05,
+    432: 710*0.10,
+    433: 710*0.20,
+    440: 710*0.15,
+    600: 710,
+    610: 710*0.15,
+    621: 56.8,
+    622: 710*0.07,
+    623: 710*0.20,
+    631: 710*0.05,
+    632: 710*0.10,
+    633: 710*0.20,
+    640: 710*0.15
+}
+
+var cetqs = {
+    410: 100,
+    421: 7,
+    422: 8,
+    423: 10,
+    431: 10,
+    432: 10,
+    433: 10,
+    440: 100,
+    610: 100,
+    621: 8,
+    622: 7,
+    623: 10,
+    631: 10,
+    632: 10,
+    633: 10,
+    640: 100
+}
+
+function autocalc(input){
+    var gradesList = [410, 421, 422, 423, 431, 432, 433, 440, 610, 621, 622, 623, 631, 632, 633, 640]
+    var grades = {}
+    for(var i=0; i<gradesList.length; i++){
+        grades[gradesList[i]] = (input[gradesList[i]] / cetqs[gradesList[i]]) * cet[gradesList[i]]
+    }
+    
+    return grades
+}
+
+function setOutputs(arr){
+    var gradesList = [410, 421, 422, 423, 431, 432, 433, 440, 610, 621, 622, 623, 631, 632, 633, 640, 400, 600]
+    for(var i=0; i<gradesList.length; i++){
+        if(gradesList[i] == 400){
+            setOutputValue(
+                400,
+                arr[410] + arr[421] + arr[422] + arr[423] + arr[431] + arr[432] + arr[433] + arr[440])
+        }else if(gradesList[i] == 600){
+            setOutputValue(
+                600,
+                arr[610] + arr[621] + arr[622] + arr[623] + arr[631] + arr[632] + arr[633] + arr[640])
+        }else{
+            setOutputValue(gradesList[i], arr[gradesList[i]])
+        }
+    }
+}
+
+function setOutputValue(id, value){
+    //if(value
+    $('#g' + id).text(value)
+}
+
+function getInputs(){
+    var inputsList = [410, 421, 422, 423, 431, 432, 433, 440, 610, 621, 622, 623, 631, 632, 633, 640]
+    var input = {}
+    for(var i=0; i<inputsList.length; i++){
+        input[inputsList[i]] = getInputValue(inputsList[i])
+    }
+    return input
+}
+
+function getInputValue(id){
+    var v = Math.round(Number($('#cet' + id).val()))
+    if(isNaN(v)){ return 0 }
+    return v
+}
+
+function onInputChange(){
+    setOutputs(autocalc(getInputs()))
+}
+
+$('input').change(onInputChange)
+$('input').keydown(onInputChange)
+$('input').keyup(onInputChange)
---
+++ cetcalc/index.php
@@ -0,0 +1,109 @@
+<!DOCYPTE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="renderer" content="webkit">
+	<meta http-equiv="Cache-Control" content="no-siteapp"/>
+	<link rel="stylesheet" href="https://jcdn-1251131545.file.myqcloud.com/amazeui/css/amazeui.min.css">
+	<script src="https://jcdn-1251131545.file.myqcloud.com/jquery2.min.js"></script>
+	<script src="https://jcdn-1251131545.file.myqcloud.com/amazeui/js/amazeui.min.js"></script>
+	
+	<style>
+	table{
+        width: 100%;
+        border: 3px solid black;
+        text-align: center;
+    }
+    
+    td{
+        padding-top: .3em;
+        padding-bottom: .3em;
+        font-size: 1.15em;
+        border-left: 2px solid black;
+    }
+    
+    thead td, table .sumstat{
+        background-color: #e0e0e0;
+        font-weight: bold;
+        border-top: 3px solid black;
+        border-bottom: 3px solid black;
+    }
+    
+    tbody td{
+        border-bottom: 1px solid black;
+    }
+    
+    tbody .classtd{
+        border-bottom: 2px solid black;
+    }
+    
+    tbody input{
+        width: 3em;
+        text-align: center;
+    }
+	</style>
+	<?php $v_href = 'cetcalc'; ?>
+	<?php require_once(dirname(dirname(__file__)) . '/index-json.php'); ?>
+	<title><?php v_title($v_href); ?> | <?php v_commonTitle($v_href); ?></title>
+</head>
+<body>
+    <div class="am-g am-g-fixed">
+	    <div class="am-u-md-2">&nbsp;</div>
+	    <div class="am-u-md-8 am-u-sm-12 main">
+		    <h1 id="title"><?php v_name($v_href); ?></h1>
+			<ol class="am-breadcrumb">
+                <li><a href="https://josephcz.win/">博客</a></li>
+				<?php if($v_href){ ?>
+                    <li><a href=".."><?php v_title(false); ?></a></li>
+                    <li class="am-active"><?php v_name($v_href); ?></li>
+				<?php }else{ ?>
+				    <li class="am-active"><?php v_name(false); ?></li>
+				<?php } ?>
+            </ol>
+		    <hr>
+			<b>作文/翻译请填写 100 分制分数<br><br></b>
+            
+			<div class="am-tabs">
+			    <ul class="am-tabs-nav am-nav am-nav-tabs">
+				    <li id="tabSwitch-0" class="am-active"><a href="javascript: void(0)" onclick="switchTab(0);">CET-4</a></li>
+				    <li id="tabSwitch-1"><a href="javascript: void(0)" onclick="switchTab(1);">CET-6</a></li>
+				</ul>
+				
+				<div class="am-tabs-bd">
+				    <div class="am-tab-panel am-active" id="tab-0">
+					    <?php require_once(dirname(__file__).'/cet4.layout.html'); ?>
+					</div>
+					
+					<div class="am-tab-panel" id="tab-1">
+					    <?php require_once(dirname(__file__).'/cet6.layout.html'); ?>
+					</div>
+				</div>
+			</div>
+            
+            <?php require_once(dirname(__file__).'/related-links.layout.html'); ?>
+			
+			<script>
+			function switchTab(tabId){
+				if(tabId == 0){
+					$('#tab-0').addClass('am-active');
+					$('#tab-1').removeClass('am-active');
+					$('#tabSwitch-0').addClass('am-active');
+					$('#tabSwitch-1').removeClass('am-active');
+				}else{
+					$('#tab-1').addClass('am-active');
+					$('#tab-0').removeClass('am-active');
+					$('#tabSwitch-1').addClass('am-active');
+					$('#tabSwitch-0').removeClass('am-active');
+				}
+			}
+			
+			</script>
+            <script src="autocalc.js"></script>
+			
+		</div> 
+	    <div class="am-u-md-2">&nbsp;</div>
+	</div>
+</body>
+</html>
